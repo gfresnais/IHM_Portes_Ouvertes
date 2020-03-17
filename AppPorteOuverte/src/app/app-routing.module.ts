72505e1b63c621595a858483ffb7b11dd4eb5a32
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-03-17 15:21:28 +0100

    Pages validerVisite et monItineraire

--- AppPorteOuverte/src/app/app-routing.module.ts
+++ AppPorteOuverte/src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'valider-visite',
+    loadChildren: () => import('./valider-visite/valider-visite.module').then( m => m.ValiderVisitePageModule)
+  },
+  {
+    path: 'mon-itineraire',
+    loadChildren: () => import('./mon-itineraire/mon-itineraire.module').then( m => m.MonItinerairePageModule)
+  },
 ];
 
 @NgModule({
---
+++ AppPorteOuverte/src/app/mon-itineraire/mon-itineraire.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-itineraire',
+  templateUrl: './mon-itineraire.page.html',
+  styleUrls: ['./mon-itineraire.page.scss'],
+})
+export class MonItinerairePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
