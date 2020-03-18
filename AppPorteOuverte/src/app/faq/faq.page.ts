3e9e4111d8061c99e2a2e2f457e96034139d7fc5
Pointet Nicolas <nicolas.91.pointet@hotmail.fr>
2020-03-18 15:37:37 +0100

    pageFAQ

--- AppPorteOuverte/src/app/faq/faq.page.ts
+++ AppPorteOuverte/src/app/faq/faq.page.ts
@@ -16,7 +16,8 @@ export class FAQPage implements OnInit {
     {question: "Jensim ?",reponse: "La meilleur assos du monde",categorie: "Vie de l'ecole"},
     {question: "Des endroits ou sortir au Mans ?",reponse: "euh... Joker",categorie: "Vie etudiante"},
     {question: "Des soirées ?",reponse: "Nos assos en font pleins pour toi ;)",categorie: "Vie etudiante"},
-    {question: "J'ai déjá fait un stage en bts, dois-je en refaire un ?",reponse: "tu peux toujours prier",categorie: "Stage"},
+    {question: "J'ai déjá fait un stage en BTS, dois-je en refaire un ?",reponse: "tu peux toujours prier",categorie: "Stage"},
+    {question : "Lorem ipsum dolor sit amet ?",reponse: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",categorie: "test"}
   ]
 
   constructor() { }
