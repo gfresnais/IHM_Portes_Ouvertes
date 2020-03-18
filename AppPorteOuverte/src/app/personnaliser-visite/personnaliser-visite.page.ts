8007c5527609d6deeb57a2f5ed8e0f5aaf051724
Abdou <abdouzozo.14@gmail.com>
2020-03-18 14:21:36 +0100

    Personnaliser Visite update

--- AppPorteOuverte/src/app/personnaliser-visite/personnaliser-visite.page.ts
+++ AppPorteOuverte/src/app/personnaliser-visite/personnaliser-visite.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personnaliser-visite',
-  templateUrl: './personnaliser-visite.page.html',
+    templateUrl: './personnaliser-visite.page.html',
   styleUrls: ['./personnaliser-visite.page.scss'],
 })
 export class PersonnaliserVisitePage implements OnInit {
