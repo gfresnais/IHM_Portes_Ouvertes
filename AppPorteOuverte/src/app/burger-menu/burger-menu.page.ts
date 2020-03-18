a07be140714ad33ade76cc79242b7f6b23224be2
Roxane <roxane.le_tensorer.etu@univ-lemans.fr>
2020-03-18 16:46:35 +0100

    Tentative de création d'un menu

--- AppPorteOuverte/src/app/burger-menu/burger-menu.page.ts
+++ AppPorteOuverte/src/app/burger-menu/burger-menu.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-burger-menu',
-  templateUrl: './burger-menu.page.html',
+  selector: 'burger-menu',
+  templateUrl: 'burger-menu.page.html',
   styleUrls: ['./burger-menu.page.scss'],
 })
 export class BurgerMenuPage {
@@ -13,14 +13,5 @@ constructor(private menu: MenuController) { }
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
-  }
-
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
   }
 }
