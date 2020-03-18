ab6beb9a10e55cfd1a8ae8fab47d3b1a1f190e95
lilian vallée <lilian.vallee1108@gmail.com>
2020-03-18 16:37:35 +0100

    renseignement 0.4

--- AppPorteOuverte/src/app/renseignement/renseignement.module.ts
+++ AppPorteOuverte/src/app/renseignement/renseignement.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { RenseignementPageRoutingModule } from './renseignement-routing.module';
 
 import { RenseignementPage } from './renseignement.page';
+import { ExpandableComponent } from "../components/expandable/expandable.component";
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { RenseignementPage } from './renseignement.page';
     IonicModule,
     RenseignementPageRoutingModule
   ],
-  declarations: [RenseignementPage]
+  declarations: [RenseignementPage, ExpandableComponent]
 })
 export class RenseignementPageModule {}
--- AppPorteOuverte/src/app/renseignement/renseignement.page.ts
+++ AppPorteOuverte/src/app/renseignement/renseignement.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-renseignement',
   templateUrl: './renseignement.page.html',
   styleUrls: ['./renseignement.page.scss'],
 })
-export class RenseignementPage implements OnInit {
+export class RenseignementPage {
 
-  constructor() { }
+public items: any = [];
 
-  ngOnInit() {
+  constructor() {
+    this.items = [
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false },
+      { expanded: false }
+    ];
   }
 
+  expandItem(item): void {
+    if (item.expanded) {
+      item.expanded = false;
+    } else {
+      this.items.map(listItem => {
+        if (item == listItem) {
+          listItem.expanded = !listItem.expanded;
+        } else {
+          listItem.expanded = false;
+        }
+        return listItem;
+      });
+    }
+  }
 }
