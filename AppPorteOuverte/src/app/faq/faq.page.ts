73c0961d5a54a8752ca5583a35d1bafcace55304
Pointet Nicolas <nicolas.91.pointet@hotmail.fr>
2020-03-18 16:03:10 +0100

    pageFAQ

--- AppPorteOuverte/src/app/faq/faq.page.ts
+++ AppPorteOuverte/src/app/faq/faq.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { info } from './info';
+import { info, cate } from './info';
 
 @Component({
   selector: 'app-faq',
@@ -8,16 +8,23 @@ import { info } from './info';
 })
 export class FAQPage implements OnInit {
 
+  listCate: cate[] = [
+    {categorie: "Internationnal"},
+    {categorie: "Vie etudiante"},
+    {categorie: "Autre"},
+    {categorie: "Stage"}
+  ]
+
   listInfo: info[] = [
-    {question: "Quels sont les partenaires de l'ensim ?",reponse: "ils existent",categorie: "International"},
-    {question: "Le stage est-il obligatoire ?",reponse: "Oui",categorie: "International"},
-    {question: "Puis-je partir ou je veux ?",reponse: "Dans la limite des places disponible",categorie: "International"},
-    {question: "Y-a t'il des associations ?",reponse: "oui",categorie: "Vie de l'ecole"},
-    {question: "Jensim ?",reponse: "La meilleur assos du monde",categorie: "Vie de l'ecole"},
-    {question: "Des endroits ou sortir au Mans ?",reponse: "euh... Joker",categorie: "Vie etudiante"},
-    {question: "Des soirées ?",reponse: "Nos assos en font pleins pour toi ;)",categorie: "Vie etudiante"},
-    {question: "J'ai déjá fait un stage en BTS, dois-je en refaire un ?",reponse: "tu peux toujours prier",categorie: "Stage"},
-    {question : "Lorem ipsum dolor sit amet ?",reponse: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",categorie: "test"}
+    {question: "Quels sont les partenaires de l'ensim ?",reponse: "ils existent",categorie: cate[0]},
+    {question: "Le stage est-il obligatoire ?",reponse: "Oui",categorie: cate[0]},
+    {question: "Puis-je partir ou je veux ?",reponse: "Dans la limite des places disponible",categorie: cate[0]},
+    {question: "Y-a t'il des associations ?",reponse: "oui",categorie: cate[1]},
+    {question: "Jensim ?",reponse: "La meilleur assos du monde",categorie: cate[1]},
+    {question: "Des endroits ou sortir au Mans ?",reponse: "euh... Joker",categorie: cate[1]},
+    {question: "Des soirées ?",reponse: "Nos assos en font pleins pour toi ;)",categorie: cate[1]},
+    {question: "J'ai déjá fait un stage en BTS, dois-je en refaire un ?",reponse: "tu peux toujours prier",categorie: cate[3]},
+    {question : "Lorem ipsum dolor sit amet ?",reponse: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",categorie: cate[2]}
   ]
 
   constructor() { }
--- AppPorteOuverte/src/app/faq/info.ts
+++ AppPorteOuverte/src/app/faq/info.ts
@@ -1,5 +1,9 @@
 export class info {
     question:string;
     reponse:string;
+    categorie:cate;
+}
+
+export class cate {
     categorie:string;
 }
