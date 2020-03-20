28797f2d4ceffaf046335adf76ceb34bfeeaf703
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-03-20 15:14:28 +0100

    Liste cards Itinéraire dynamique

--- AppPorteOuverte/src/app/mon-itineraire/mon-itineraire.page.ts
+++ AppPorteOuverte/src/app/mon-itineraire/mon-itineraire.page.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonItinerairePage implements OnInit {
 
+  listCards: MyCard[] = [{ titre: "TD1 Réamix", sous_titre: "1er étage", 
+                          description: "Présentation des projets de réalité virtuelle", 
+                          visite: true
+                        },
+                        {
+                          titre: "C03 Présentation de l'école", sous_titre: "Rez-de-chaussée", 
+                          description: "Présentation de l'école et du protocole d'admission", 
+                          visite: false
+                        },
+                        {
+                          titre: "TD4 Projet 2A", sous_titre: "1er étage", 
+                          description: "Présentation des projets de deuxième année", 
+                          visite: false
+                        }];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class MyCard {
+  titre: string;
+  sous_titre: string;
+  description: string;
+  visite: boolean;
 }
