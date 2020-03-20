b04de70c2f82c63666fbd02138643fbaa5cc4e07
Sofien Derbel <36224236+SofienDerbel@users.noreply.github.com>
2020-03-20 13:23:30 +0100

    localisation
    sur IOS ca marche
    sur android non

--- AppPorteOuverte/src/app/se-reperer/se-reperer.page.ts
+++ AppPorteOuverte/src/app/se-reperer/se-reperer.page.ts
@@ -1,41 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import { Map, latLng, tileLayer, Layer, marker, polyline, LatLng } from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 @Component({
   selector: 'app-se-reperer',
   templateUrl: './se-reperer.page.html',
   styleUrls: ['./se-reperer.page.scss'],
 })
-export class SeRepererPage implements OnInit {
+export class SeRepererPage  {
 
-
-  ngOnInit() {
+map: Map;
+marker:any;
+latlong= [];
+  
+  
+  constructor(private geolocation:Geolocation) {}
+  ionViewDidEnter(){
+    this.showMap();
   }
-
-  map: Map;
-  pickupLocation: string;
-  constructor(private router:Router) {}
-  onpickupClick(){
-    this.router.navigate(['./pickup-location']);
+  getPositions(){
+    this.geolocation.getCurrentPosition({enableHighAccuracy : true }).then((res)=>{
+     return this.latlong=[res.coords.latitude,res.coords.longitude]
+    }).then((latLng)=>{
+      this.showMarker(latLng);
+    });
   }
-  ionViewDidEnter() { this.leafletMap(); }
-
-  leafletMap() {
-    // In setView add latLng and zoom
-    this.map = new Map('mapId').setView([39.9075000, 116.3972300], 10);
-    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'edupala.com © ionic LeafLet',
-    }).addTo(this.map);
-
-
-    marker([28.6, 77]).addTo(this.map)
-      .bindPopup('Ionic 4 <br> Leaflet.')
-      .openPopup();
-  }
-
-  /** Remove map when we have multiple map object */
-  ionViewWillLeave() {
-    this.map.remove();
+showMap(){
+  this.map = new Map('myMap').setView([0.199556,48.00611],10);
+  tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
+}
+  showMarker(latlong){
+    this.marker = marker(latlong);
+    this.marker.addTo(this.map).bindPopup('Hey, i am here');
   }
 
 }
