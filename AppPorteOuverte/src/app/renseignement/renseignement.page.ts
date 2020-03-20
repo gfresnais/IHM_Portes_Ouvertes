cb802c3ea039e6f571410653af5a8b989d73163a
lilian vallée <lilian.vallee1108@gmail.com>
2020-03-20 15:35:53 +0100

    renseignement 0.4.3

--- AppPorteOuverte/src/app/renseignement/renseignement.page.ts
+++ AppPorteOuverte/src/app/renseignement/renseignement.page.ts
@@ -8,27 +8,49 @@ import { Component } from '@angular/core';
 })
 export class RenseignementPage {
 
-public items: any = [];
+  public infos: Info[];
+  public i1: Info;
+  public i2: Info;
+  public i3: Info;
 
   constructor() {
-    this.items = [
-      { expanded: false },
-      { expanded: false },
-      { expanded: false },
+
+    this.i1.title = "Information sur l'école";
+    this.i1.list = [
+      "Dépliant de l'école",
+      "Dépliant du concours Geipei Polytech",
+      "Dépliant cycle ingénieur",
+      "Dépliant Khâgne"
     ];
+    this.i2.title = "Information sur l'école";
+    this.i2.list = [
+      "Dépliant de l'école",
+      "Dépliant du concours Geipei Polytech",
+      "Dépliant cycle ingénieur",
+      "Dépliant Khâgne"
+    ];
+    this.i3.title = "Information sur l'école";
+    this.i3.list = [
+      "Dépliant de l'école",
+      "Dépliant du concours Geipei Polytech",
+      "Dépliant cycle ingénieur",
+      "Dépliant Khâgne"
+    ];
+
+    this.infos = [this.i1, this.i2, this.i3];
   }
 
-  expandItem(item): void {
-    if (item.expanded) {
-      item.expanded = false;
+  expandItem(info): void {
+    if (info.expanded) {
+      info.expanded = false;
     } else {
-      item.expanded = true;
+      info.expanded = true;
     }
   }
 }
 
-export class item {
-
-  public title:string;
-
+export class Info {
+  public title: string = "titre";
+  public expendable: boolean = false;
+  public list: string[];
   }
