584b18a725cd92367894bd8564eee1d18fb73e0c
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-03-24 11:30:39 +0100

    Ajout/Suppression ateliers

--- AppPorteOuverte/src/app/personnaliser-visite/personnaliser-visite.page.ts
+++ AppPorteOuverte/src/app/personnaliser-visite/personnaliser-visite.page.ts
@@ -6,29 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personnaliser-visite.page.scss'],
 })
 export class PersonnaliserVisitePage implements OnInit {
-   tabAtelier: AtelierCard[]; 
-  //  = [{
-  //   title: "Atelier 1",
-  //   emp: "1er étage",
-  //   desc: "Présentation des projets de réalité virtuelle"
-  // },
-  // {
-  //   title: "Atelier 2",
-  //   emp: "Rez-de-chaussée",
-  //   desc: "Présentation de l’école et du protocole d’admission"
-  // },
-  // {
-  //   title: "Atelier 3",
-  //   emp: "1er étage",
-  //   desc: "Présentation des projets de deuxième année"
-  // }];
-
+  
+  // Liste des ateliers
+  tabAtelier: AtelierCard[] = []; 
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  /**
+   * Ajoute un atelier au tableau
+   * @param numAtelier 
+   */
   addVisit(numAtelier: number) {
     switch (numAtelier) {
       case 1:
@@ -47,6 +37,17 @@ export class PersonnaliserVisitePage implements OnInit {
                               desc: "Présentation des projets de deuxième année"});
         break;
     }
+  }
+
+  /**
+   * Supprime une entrée du tableau à partir d'un titre
+   * @param title 
+   */
+  removeVisit(title: string) {
+    // Trouve l'index dans le tableau de la première occurrence du titre
+    let index = this.tabAtelier.findIndex(x => x.title === title);
+    // Supprime l'entrée à partir de l'index
+    this.tabAtelier.splice(index, 1);
   }
 }
 
