7db30c837267df3fd2ea367f65b24dc0f0b4bdd7
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-03-24 10:29:26 +0100

    Valeurs différentes guides + lien mort home

--- AppPorteOuverte/src/app/valider-visite/valider-visite.page.ts
+++ AppPorteOuverte/src/app/valider-visite/valider-visite.page.ts
@@ -8,13 +8,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ValiderVisitePage implements OnInit {
 
+  // Valeurs possibles : prof, etud, virtual
   id: string;
+  // Guide
+  guide: string;
+  // Nom du guide
+  name: string;
+  // Promo ou fonction
+  fonc: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     // Set id value to a get parameter from a route
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    switch (this.id) {
+      case "prof":
+        this.guide = "Un prof";
+        this.name = "Aim Ine";
+        this.fonc = "Sécurité Informatique";
+        break;
+      case "virtual":
+        this.guide = "Guide virtuel";
+        this.name = "Bip Boop";
+        this.fonc = "Guide virtuel";
+        break;
+      default:
+        this.guide = "Un élève";
+        this.name = "Théo Guidon";
+        this.fonc = "4A Info";
+        break;
+    }
   }
 
 }
