1c11c7a4cf9d95ace73afb225f4f134a1c2e6457
lilian vallée <lilian.vallee1108@gmail.com>
2020-03-24 11:12:54 +0100

    Renseignement 1.0
    Fini!!!
    il reste le style

--- AppPorteOuverte/src/app/renseignement/renseignement.page.ts
+++ AppPorteOuverte/src/app/renseignement/renseignement.page.ts
@@ -13,26 +13,38 @@ export class RenseignementPage {
     {
       titre: "Informations sur l'école",
       expendable: false,
-      lien1: "Dépliant de l'école",
-      lien2: "Dépliant du concours Geipi Plytech",
-      lien3: "Dépliant cycle ingénieur",
-      lien4: "Dépliant Khâgne",
+      st1: "Dépliant de l'école",
+      lien1: "http://ensim.univ-lemans.fr/_resources/documents/Plaquette%2520ENSIM%25202020%2520web.pdf?download=true",
+      st2: "Dépliant du concours Geipi Plytech",
+      lien2: "http://www.demain-ingenieur.fr/wp-content/uploads/Concours-Polytech-Basse-d%C3%A9finition2.pdf",
+      st3: "Dépliant cycle ingénieur",
+      lien3: "http://ensim.univ-lemans.fr/_resources/documents/Depliant%2520A5%2520Programme%2520de%2520formation%2520classique.pdf?download=true",
+      st4: "Dépliant Khâgne",
+      lien4: "http://ensim.univ-lemans.fr/_resources/Depliant_%2520Khagne_%2520BL.pdf?download=true",
     },
     {
       titre: "satistiques",
       expendable: false,
-      lien1: "Débouchées",
-      lien2: "Salaires",
-      lien3: "Entreprises",
-      lien4: "Etudiants",
+      st1: "Débouchées",
+      lien1: "http://ensim.univ-lemans.fr/fr/espace-entreprises/insertion-professionnelle.html",
+      st2: "Stage",
+      lien2: "http://ensim.univ-lemans.fr/fr/espace-entreprises/insertion-professionnelle/stages.html",
+      st3: "Entreprises",
+      lien3: "http://ensim.univ-lemans.fr/fr/espace-entreprises/insertion-professionnelle/metiers.html",
+      st4: "Etudiants",
+      lien4: "http://ensim.univ-lemans.fr/fr/espace-entreprises/insertion-professionnelle/parcours-d-ensimiens.html",
     },
     {
       titre: "Programme des portes ouvertes",
       expendable: false,
-      lien1: "Conférences",
-      lien2: "Ateliers",
-      lien3: "Stands",
-      lien4: "Restauration",
+      st1: "Conférences",
+      lien1: "",
+      st2: "Ateliers",
+      lien2: "",
+      st3: "Stands",
+      lien3: "",
+      st4: "Restauration",
+      lien4: "",
     },
   ]
 
@@ -62,8 +74,12 @@ export class RenseignementPage {
 export class Info {
   public titre: string;
   public expendable: boolean;
+  public st1: string;
   public lien1: string;
+  public st2: string;
   public lien2: string;
+  public st3: string;
   public lien3: string;
+  public st4: string;
   public lien4: string;
   }
